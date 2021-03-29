adf6a0c58ec1da1e5732c4a181f17c81a4d1c1bc
Andrea Staskova <andrea@intiveo.com>
2021-03-29 16:46:50 +0200

    function for checking position of hunter and coin

--- main.js
+++ main.js
@@ -7,16 +7,23 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
-let hunter = document.getElementById("panacek");
 
+// zatím je hunter globání, možná změnit?
+let hunter = document.getElementById("panacek");
+let coin = document.getElementById("mince");
 
 function move(udalost) {
-	//console.log(udalost);
+	
 	let startTop = parseInt((window.getComputedStyle(hunter)).top);
 	let startLeft = parseInt((window.getComputedStyle(hunter)).left);
 
+	// tahle nested condition mi přijde trochu prasácká, možná to jde i nějak jednodušeji
 	if (udalost.key == "ArrowDown") {
-		hunter.style.top = (startTop + 15) + "px";
+		if (startTop < (window.innerHeight - 70)) {
+			hunter.style.top = (startTop + 15) + "px";
+		} else {
+			hunter.style.top = (window.innerHeight - 70) + "px";
+		}
 	} else if (udalost.key == "ArrowUp") {
 		if (startTop > 0) {
 			hunter.style.top = (startTop - 15) + "px";
@@ -30,24 +37,41 @@ function move(udalost) {
 			hunter.style.left = "0px";
 		}	
 	} else if (udalost.key == "ArrowRight") {
-		hunter.style.left = (startLeft + 15) + "px";
+		if (startLeft < (window.innerWidth - 64)) {
+			hunter.style.left = (startLeft + 15) + "px";
+		} else {
+			hunter.style.left = (window.innerWidth - 64) + "px";
+		}
 	} else {
 		alert("Use arrow keys to move the hunter.");
 	}
 	//console.log(startTop);
 	//console.log(startLeft);
-	
 	}
+
 /**
-ještě upravit aby se mince nedostávala na hranu window/view
  * 
- * @param {*} elementSelector 
+ * @param {string} elementSelector 
  */	
 function randomPlace(elementSelector) {
-	let coin = document.getElementById(elementSelector);
-	maxTop = window.innerHeight;
-	maxLeft = window.innerWidth;
-	coin.style.top = (Math.random() * maxTop) + "px"
-	coin.style.left = (Math.random() * maxLeft) + "px"
-
+	document.getElementById(elementSelector).style.top = (Math.random() * (window.innerHeight - 36)) + "px";
+	document.getElementById(elementSelector).style.left = (Math.random() * (window.innerWidth - 36)) + "px";
 }	
+
+function checkMove() {
+	let hunterY = parseInt((window.getComputedStyle(hunter)).top);
+	let hunterX = parseInt((window.getComputedStyle(hunter)).left);
+
+	let coinX = parseInt((window.getComputedStyle(coin)).left);
+	let coinY = parseInt((window.getComputedStyle(coin)).top);
+
+	//console.log("hunter: ", hunterX, hunterY);
+	//console.log("coin: ", coinX, coinY);
+	//console.log("X: ", hunterX - coinX, "Y: ", hunterY - coinY)
+
+	if ((hunterX - coinX) >= -40 && (hunterX - coinX) <= 15 && (hunterY - coinY) >= -40 && (hunterY - coinY) <= 15 ) {
+		console.log("Score!");
+		randomPlace("mince");
+	}
+	
+}
